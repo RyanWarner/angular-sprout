5747ad52c4615dec77fd41450bf9b780fb1e1692
Ryan Warner <warner.ryan@gmail.com>
2015-04-03 11:41:47 -0700

    misplaced semicolon

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var gulp        = require( 'gulp' );
-var requireDir  = require( 'require-dir' )
-var runSequence = require( 'run-sequence' );;
+var requireDir  = require( 'require-dir' );
+var runSequence = require( 'run-sequence' );
 
 requireDir( './gulp/tasks', { recurse: true } );
 
